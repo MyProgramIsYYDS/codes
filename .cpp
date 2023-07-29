4a05567a0822fe0480b3f9ffada0780fff715b88
MyProgramIsYYDS <89306238+MyProgramIsYYDS@users.noreply.github.com>
2023-07-29 18:12:59 +0800

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,1 +1,8 @@
-
+#include<bits/stdc++.h>
+using namespace std;map<string,string>m;main(){
+    m["P4118"]="https://vjudge.net/solution/44505344/NeNV00CItwTRRUvfj9m4";
+    m["P7906"]="https://vjudge.net/solution/43885938/HCr9cACOFGdATWRhmQ0U";
+    m["P8335"]="https://vjudge.net/solution/43664391/Kwv82uTIEnpvciRCJ3mH";
+    m["SP26045"]="https://vjudge.net/solution/43506069/htg6fv2yJTZyMsIDaJc7";
+    m["SP33046"]="https://vjudge.net/solution/42919691/Rem4Irk59PlLjiuqkT5g";
+string s;cin>>s;cout<<m[s];}
